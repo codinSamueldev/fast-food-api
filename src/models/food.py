c691ab551eac2dcf894a028a5daa93da53b7f164
Samuel Henao <codinsamueldev@gmail.com>
2024-02-02 18:52:00 -0500

    new: meals model

---
+++ src/models/food.py
@@ -0,0 +1,11 @@
+from config.database import Base
+from sqlalchemy import Column, Integer, Float, String
+
+class Meal(Base):
+
+    __tablename__ = "Meals"
+
+    meal_id = Column(Integer, primary_key=True)
+    meal_name = Column(String)
+    meal_ingredients = Column(String)
+    meal_price = Column(Float)
