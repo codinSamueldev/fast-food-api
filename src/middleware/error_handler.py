de89a44aa166d83e3f3e7829b4fc53b40dbfbd8e
Samuel Henao <codinsamueldev@gmail.com>
2024-02-03 13:07:47 -0500

    new: add middleware to handle exceptions

---
+++ src/middleware/error_handler.py
@@ -0,0 +1,18 @@
+from starlette import status
+from starlette.middleware.base import BaseHTTPMiddleware, DispatchFunction, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.types import ASGIApp
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+class ErrorHandler(BaseHTTPMiddleware):
+    def __init__(self, app: FastAPI) -> None:
+        super().__init__(app)
+
+    async def dispatch(self, request: Request, call_next) -> Response | JSONResponse:
+        try:
+            return await call_next(request)
+        except Exception as e:
+            return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+                                content=str(e))
