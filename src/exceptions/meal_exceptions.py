b8f8aa9bb7aa5d89579d7b8093d388005495caa6
Samuel Henao <codinsamueldev@gmail.com>
2024-02-03 14:55:28 -0500

    create new exception

--- src/exceptions/meal_exceptions.py
+++ src/exceptions/meal_exceptions.py
@@ -1,3 +1,4 @@
 from fastapi import HTTPException
 
 MEAL_ID_EXCEPTION = HTTPException(status_code=404, detail="Meal ID not found :c")
+NOT_FOUND_EXCEPTION = HTTPException(status_code=404, detail="No se encontró información en la base de datos.")
