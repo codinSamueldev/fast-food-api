206028d5cfb198bb84a61872c006c77fbe77597c
Samuel Henao <codinsamueldev@gmail.com>
2024-02-03 14:57:07 -0500

    fix: typo on post method create new get method by id, and fix return response for all meals method

--- src/routers/meals.py
+++ src/routers/meals.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from starlette import status
 
 from services.meal_services import MealMethods
@@ -12,15 +13,19 @@ DB = local_session()
 meals_router = APIRouter(prefix="/food", tags=["MEALS"])
 
 
-@meals_router.get("/", status_code=200, response_model=Food)
-def get_meals(): return MealMethods(DB).get_all_meals_available()
+@meals_router.get("/", status_code=status.HTTP_200_OK, response_model=Food)
+def get_meals(): return JSONResponse(content=jsonable_encoder(MealMethods(DB).get_all_meals_available()), status_code=status.HTTP_200_OK)
+
+
+@meals_router.get("/{id}", status_code=status.HTTP_200_OK)
+def get_meal(id: int): return JSONResponse(content=jsonable_encoder(MealMethods(DB).get_meal(id)), status_code=status.HTTP_200_OK)
 
 
 @meals_router.post("/", status_code=201)
 def add_meal(new_meal: Food):
     MealMethods(DB).add_new_meal(new_meal_data=new_meal)
 
-    return JSONResponse(content={"message:" "Se ha añadido una nueva comida!"}, status_code=201)
+    return JSONResponse(content={"message": "Se ha añadido una nueva comida!"}, status_code=201)
 
 
 @meals_router.delete("/", status_code=status.HTTP_200_OK)
