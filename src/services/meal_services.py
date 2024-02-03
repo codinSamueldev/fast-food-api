6d00931714acbcfcee9c528d0c52ea46497269c5
Samuel Henao <codinsamueldev@gmail.com>
2024-02-03 13:13:27 -0500

    feat: add two services, one to add meals, the other to delete them all

--- src/services/meal_services.py
+++ src/services/meal_services.py
@@ -1,4 +1,8 @@
-from models.food import Meal
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from starlette import status
+from models.food import Meal as MealModel
+from schemas.meal_pydantic_model import Food
 from exceptions.meal_exceptions import MEAL_ID_EXCEPTION
 
 
@@ -8,16 +12,30 @@ class MealMethods:
 
     
     def get_all_meals_available(self):
-        all_meals = self.db.query(Meal).all()
+        all_meals = self.db.query(MealModel).all()
+
+        if not all_meals:
+            return JSONResponse(content={"message": "No se encontró información en la base de datos."}, status_code=status.HTTP_404_NOT_FOUND)
 
         return all_meals
 
 
     def get_meal(self, id: int):
-        meal = self.db.query(Meal).filter(id == Meal.meal_id).first()
+        meal = self.db.query(MealModel).filter(id == MealModel.meal_id).first()
 
         if not meal:
             raise MEAL_ID_EXCEPTION
 
         return meal
 
+
+    def add_new_meal(self, new_meal_data: Food):
+        new_meal = MealModel(**jsonable_encoder(dict(new_meal_data)))
+
+        self.db.add(new_meal)
+        self.db.commit()
+
+
+    def delete_all_meals_stored(self):
+        self.db.query(MealModel).delete()
+        self.db.commit()
